f3aad8a95decbf2a89d6d6154ee79096f1da0471
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-19 14:52:45 +0000

    ..

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpBasics
 {
@@ -6,19 +7,33 @@ namespace CSharpBasics
 	{
 		static void Main(string[] args)
 		{
-			double sum = 0;
-			for (int i = 0; i < 1000; i++)
-			{
-				sum += 1 / 3.0;
-			}
-			Console.WriteLine("1/3 is: " + 1 / 3.0);
-			Console.WriteLine("1/3 added 1000 times: " + sum);
-			Console.WriteLine("1/3 multiplied by 1000: " + 1 / 3.0 * 1000);
+			//double sum = 0;
+			//for (int i = 0; i < 1000; i++)
+			//{
+			//	sum += 1 / 3.0;
+			//}
+			//Console.WriteLine("1/3 is: " + 1 / 3.0);
+			//Console.WriteLine("1/3 added 1000 times: " + sum);
+			//Console.WriteLine("1/3 multiplied by 1000: " + 1 / 3.0 * 1000);
 
-			float pi = 3.14f;
-			double dPi = pi;
-			Console.WriteLine(pi);
-			Console.WriteLine(dPi);
+			//float pi = 3.14f;
+			//double dPi = pi;
+			//decimal dePi = pi;
+			//Console.WriteLine(pi);
+			//Console.WriteLine(dPi);
+			//Console.WriteLine(dePi);
+
+			//int num = -5;
+			//ulong longNum = (ulong)num;
+			//Console.WriteLine(longNum);
+
+			//char fourChar = '4';
+			//int fourInt = (int)Char.GetNumericValue(fourChar);
+			//Console.WriteLine(fourInt);
+
+			//var names = new List<string>();
+
+
 		}
 	}
 }
