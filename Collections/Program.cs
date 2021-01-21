1652b37d60108145b2e4662520d05c8562b71c46
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-21 14:40:18 +0000

    learned about colletions such as lists, queus, stacks, hashsets and dictionaries

---
+++ Collections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//List<int> ListOfIntegers = new List<int>() { 1, 2, 3, 4, 5 };
+
+			//ListOfIntegers.Add(24);
+			//ListOfIntegers.Add(12);
+			//ListOfIntegers.Add(-16);
+			//ListOfIntegers.Insert(2, 4);
+			//ListOfIntegers.Reverse();
+			//ListOfIntegers.RemoveAt(3);
+
+			//Console.WriteLine("Items in the list:");
+			//foreach (int item in ListOfIntegers)
+			//{
+			//	Console.WriteLine(item);
+			//}
+			//Console.WriteLine();
+			//Console.WriteLine("Sorted list of integers:");
+			//ListOfIntegers.Sort();
+			//foreach (int item in ListOfIntegers)
+			//{
+			//	Console.WriteLine(item);
+			//}
+
+			Queue<string> sainsburys = new Queue<string>();
+
+
+
+		}
+	}
+}
