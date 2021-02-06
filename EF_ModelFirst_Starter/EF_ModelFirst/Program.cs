f87807be19a536722d8a6b130b9cb5b44279c5f0
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-06 22:03:16 +0000

    first commit for southwind

---
+++ EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EF_ModelFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SouthwindContext())
+            {
+                // Create
+                Console.WriteLine("Creating some customers");
+                db.Add(new Customer() { CustomerId = "MART", City = "London", ContactName = "Martin", Country = "UK", PostalCode = "SE1" });
+                db.Add(new Customer() { CustomerId = "CATH", City = "Birmingham", ContactName = "Cathy", Country = "UK", PostalCode = "AB1 4DD" });
+            
+                db.SaveChanges();
+            }
+        }
+    }
+}
