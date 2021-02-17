42d419800e4ac1a636da52ce2096781d187ebce0
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-17 15:22:43 +0000

    changed customermanager tests to use moq instead

--- DBFirst_3Layer_Refactor_Complete/NorthwindTests/CustomerManagerTests.cs
+++ DBFirst_3Layer_Refactor_Complete/NorthwindTests/CustomerManagerTests.cs
@@ -1,138 +1,131 @@
-using NUnit.Framework;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
 using NorthwindBusiness;
 using NorthwindData;
-using System.Linq;
+using NorthwindData.Services;
+using NUnit.Framework;
 
 namespace NorthwindTests
 {
-    public class CustomerTests
-    {
-        CustomerManager _customerManager;
-        [SetUp]
-        public void Setup()
-        {
-           _customerManager = new CustomerManager();
-            // remove test entry in DB if present
-            using (var db = new NorthwindContext())
-            {
-                var selectedCustomers =
-                from c in db.Customers
-                where c.CustomerId == "MAND"
-                select c;
+	public class CustomerTests
+	{
+		private CustomerManager _customerManager;
+		private NorthwindContext _db;
 
-                db.Customers.RemoveRange(selectedCustomers);
-                db.SaveChanges();
-            }
-        }
+		//[OneTimeSetUp]
+		//public void OneTimeSetup()
+		//{
+		//	var options = new DbContextOptionsBuilder<NorthwindContext>()
+		//		.UseInMemoryDatabase(databaseName: "Northwind_Fake")
+		//		.Options;
+		//	_db = new NorthwindContext(options);
+		//	_customerManager = new CustomerManager(new CustomerService(_db));
+		//}
 
-        [Test]
-        public void WhenANewCustomerIsAdded_TheNumberOfCustemersIncreasesBy1()
-        {
-            using (var db = new NorthwindContext())
-            {
-                var numberOfCustomersBefore = db.Customers.Count();
-                _customerManager.Create("MAND", "Nish Mandal", "Sparta Global");
-                var numberOfCustomersAfter = db.Customers.Count();
+		//[SetUp]
+		//public void Setup()
+		//{
+		//	// remove test entry in DB if present
+		//	var selectedCustomers =
+		//	from c in _db.Customers
+		//	where c.CustomerId == "MAND"
+		//	select c;
 
-                Assert.AreEqual(numberOfCustomersBefore + 1, numberOfCustomersAfter);
-            }
-        }
+		//	_db.Customers.RemoveRange(selectedCustomers);
+		//	_db.SaveChanges();
+		//}
 
-        [Test]
-        public void WhenANewCustomerIsCreated_TheCorrectStringIsReturned()
-        {
-            using (var db = new NorthwindContext())
-            {
-                var numberOfCustomersBefore = db.Customers.Count();
-                var result = _customerManager.Create("MAND", "Nish Mandal", "Sparta Global");
-                Assert.That(result, Does.Contain("MAND - Nish Mandal"));
-            }
-        }
+		//[Test]
+		//public void WhenANewCustomerIsAdded_TheNumberOfCustemersIncreasesBy1()
+		//{
+		//	var numberOfCustomersBefore = _db.Customers.Count();
+		//	_customerManager.Create("MAND", "Nish Mandal", "Sparta Global");
+		//	var numberOfCustomersAfter = _db.Customers.Count();
 
-        [Test]
-        public void FindById_RetrievesTheCorrectCustomer()
-        {
-            using (var db = new NorthwindContext())
-            {
-                var numberOfCustomersBefore = db.Customers.Count();
-                _customerManager.Create("MAND", "Nish Mandal", "Sparta Global");
-                var numberOfCustomersAfter = db.Customers.Count();
+		//	Assert.AreEqual(numberOfCustomersBefore + 1, numberOfCustomersAfter);
+		//}
 
-                Assert.AreEqual(numberOfCustomersBefore + 1, numberOfCustomersAfter);
-            }
-        }
 
-        [Test]
-        public void WhenACustomersDetailsAreChanged_TheDatabaseIsUpdated()
-        {
-            using (var db = new NorthwindContext())
-            {
-                // arrange
-                _customerManager.Create("MAND", "Nish Mandal", "Sparta Global", "Paris");
-                // act
-                _customerManager.Update("MAND", "Nish Mandal", "Birmingham", null, null);
-                // assert
-                var updatedCustomer = db.Customers.Find("MAND");
-                Assert.AreEqual("Birmingham", updatedCustomer.City);
-            }
-        }
+		//[Test]
+		//public void FindById_RetrievesTheCorrectCustomer()
+		//{
+		//	var numberOfCustomersBefore = _db.Customers.Count();
+		//	_customerManager.Create("MAND", "Nish Mandal", "Sparta Global");
+		//	var numberOfCustomersAfter = _db.Customers.Count();
 
-        [Test]
-        public void WhenACustomerIsDeleted_TheCustomerIsNoLongerPresent()
-        {
-            using (var db = new NorthwindContext())
-            {
-                // arrange
-                _customerManager.Create("MAND", "Nish Mandal", "Sparta Global", "Paris");
-                var createdCustomer = db.Customers.Find("MAND");
-                Assert.That(createdCustomer.ContactName, Is.EqualTo("Nish Mandal"));
-                // act
-                _customerManager.Delete("MAND");
-                // assert
-                var deletedCustomer = db.Customers.Find("MAND");
-                Assert.That(deletedCustomer, Is.Null);
-            }
-        }
+		//	Assert.AreEqual(numberOfCustomersBefore + 1, numberOfCustomersAfter);
+		//}
 
-        [Test]
-        public void WhenACustomerIsDeleted_TheCorrectStringIsReturned()
-        {
-            using (var db = new NorthwindContext())
-            {
-                // Arrange
-                _customerManager.Create("MAND", "Nish Mandal", "Sparta Global", "Paris");
-                // Act
-                var result = _customerManager.Delete("MAND");
-                // Assert
-                Assert.That(result, Is.EqualTo("Customer MAND deleted"));
+		//[Test]
+		//public void WhenACustomersDetailsAreChanged_TheDatabaseIsUpdated()
+		//{
+		//	// arrange
+		//	_customerManager.Create("MAND", "Nish Mandal", "Sparta Global", "Paris");
+		//	// act
+		//	_customerManager.Update("MAND", "Nish Mandal", "Birmingham", null, null);
+		//	// assert
+		//	var updatedCustomer = _db.Customers.Find("MAND");
+		//	Assert.AreEqual("Birmingham", updatedCustomer.City);
+		//}
 
-            }
-        }
 
-        [Test]
-        public void WhenAnAttemptIsMadeToDeleteANonExistentCustomer_AnErrorMessageIsReturned()
-        {
-            using (var db = new NorthwindContext())
-            {
-                var result = _customerManager.Delete("MAND");
-                var deletedCustomer = db.Customers.Find("MAND");
-                Assert.That(result, Is.EqualTo("Customer MAND not found"));
-            }
-        }
 
-        [TearDown]
-        public void TearDown()
-        {
-            using (var db = new NorthwindContext())
-            {
-                var selectedCustomers =
-                from c in db.Customers
-                where c.CustomerId == "MAND"
-                select c;
 
-                db.Customers.RemoveRange(selectedCustomers);
-                db.SaveChanges();
-            }
-        }
-    }
+		//[Test]
+		//public void WhenACustomerIsDeleted_TheCustomerIsNoLongerPresent()
+		//{
+		//	// arrange
+		//	_customerManager.Create("MAND", "Nish Mandal", "Sparta Global", "Paris");
+		//	var createdCustomer = _db.Customers.Find("MAND");
+		//	Assert.That(createdCustomer.ContactName, Is.EqualTo("Nish Mandal"));
+		//	// act
+		//	_customerManager.Delete("MAND");
+		//	// assert
+		//	var deletedCustomer = _db.Customers.Find("MAND");
+		//	Assert.That(deletedCustomer, Is.Null);
+		//}
+
+		[Test]
+		public void WhenANewCustomerIsCreated_TheCorrectStringIsReturned()
+		{
+			var mockCustomerService = new Mock<ICustomerService>();
+			var customerManager = new CustomerManager(mockCustomerService.Object);
+			var result = customerManager.Create("MAND", "Nish Mandal", "Sparta Global");
+			Assert.That(result, Does.Contain("MAND - Nish Mandal"));
+		}
+
+		[Test]
+		public void WhenACustomerIsDeleted_TheCorrectStringIsReturned()
+		{
+			var mockCustomerService = new Mock<ICustomerService>();
+			var customerManager = new CustomerManager(mockCustomerService.Object);
+			customerManager.Create("MAND", "Nish Mandal", "Sparta Global");
+			var result = customerManager.Delete("MAND");
+			Assert.That(result, Is.EqualTo("Customer MAND deleted"));
+		}
+
+		[Test]
+		public void WhenAnAttemptIsMadeToDeleteANonExistentCustomer_AnErrorMessageIsReturned()
+		{
+			var mockCustomerService = new Mock<ICustomerService>();
+			mockCustomerService.Setup(x => x.Delete(It.IsAny<string>())).Throws<ArgumentNullException>();
+			var customerManager = new CustomerManager(mockCustomerService.Object);
+			var result = customerManager.Delete("MAND");
+			Assert.That(result, Is.EqualTo("Customer MAND not found"));
+		}
+
+		//[TearDown]
+		//public void TearDown()
+		//{
+		//	var selectedCustomers =
+		//	from c in _db.Customers
+		//	where c.CustomerId == "MAND"
+		//	select c;
+
+		//	_db.Customers.RemoveRange(selectedCustomers);
+		//	_db.SaveChanges();
+		//}
+	}
 }
---
+++ DBFirst_3Layer_Refactor_Complete/NorthwindTests/CustomerServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NorthwindData;
+using NorthwindData.Services;
+using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindTests
+{
+	class CustomerServiceTests
+	{
+		private NorthwindContext _db;
+		CustomerService _customerService;
+
+		[OneTimeSetUp]
+		public void OneTimeSetup()
+		{
+			var options = new DbContextOptionsBuilder<NorthwindContext>()
+				.UseInMemoryDatabase(databaseName: "Northwind_Fake")
+				.Options;
+			_db = new NorthwindContext(options);
+			_customerService = new CustomerService(_db);
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			// remove test entry in DB if present
+			var selectedCustomers =
+			from c in _db.Customers
+			where c.CustomerId == "MAND"
+			select c;
+
+			_db.Customers.RemoveRange(selectedCustomers);
+			_db.SaveChanges();
+		}
+
+		[Test]
+		public void WhenACustomerIsSearchedFor_TheCorrectObjectIsReturned()
+		{
+			// Arrange
+			_customerService.Add(new Customer() { CustomerId = "MAND", 
+				ContactName = "Nish Mandal", 
+				CompanyName = "Sparta Global", 
+				City = "Paris" });
+			// Act
+			var result = _customerService.GetCustomerById("MAND");
+			// Assert
+			Assert.That(result.ContactName, Is.EqualTo("Nish Mandal"));
+
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			var selectedCustomers =
+			from c in _db.Customers
+			where c.CustomerId == "MAND"
+			select c;
+
+			_db.Customers.RemoveRange(selectedCustomers);
+			_db.SaveChanges();
+		}
+
+	}
+}
